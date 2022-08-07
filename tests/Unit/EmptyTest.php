7a343e2f6d8b1c474343e74d03720a130c61b95c
Rafael Camison <rafaelcamison@gmail.com>
2022-08-07 20:07:48 +0200

    add placeholder unit test

---
+++ tests/Unit/EmptyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class EmptyTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_placeholder()
+    {
+        $this->assertTrue(true);
+    }
+}
