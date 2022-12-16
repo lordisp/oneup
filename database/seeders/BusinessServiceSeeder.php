aeb7c3beec7fe4791a2a46dcb1e039de48a6a962
Rafael Camison <rafaelcamison@gmail.com>
2022-12-16 05:22:00 +0100

    #569 update business-service-seeder

--- database/seeders/BusinessServiceSeeder.php
+++ database/seeders/BusinessServiceSeeder.php
@@ -11,15 +11,17 @@ class BusinessServiceSeeder extends Seeder
         'LHG_AZUREFOUNDATION_P',
         'LHG_GOVERNOR_P',
         'LHG_PAM_P',
-        'LHG_SOAR_P',
-        'LHG_MISP_P',
         'LHG_CDC_P',
         'LHG_SIEM_P',
         'LHG_LHGROUPAD_P',
-        'LHG_PKIUSERCERTSYNC_P',
-        'LHG_PASSWORDSYNC_P',
-        'LHG_DIRXML_P',
         'LHG_MFACDS_P',
+        'LHG_PASSWORDSYNC_P',
+        'LHG_LHGROUPAD_P',
+        'LHG_GOVMEMLHGROUPAD_P',
+        'LHG_PASSWORDSYNC_P',
+        'LHG_PASSWORDSYNCCHECK_P',
+        'LHG_CDSPASSWORDCHANGE_P',
+        'LHG_MIM_P',
     ];
 
     /**
