cf9dc9cf88b754b85592a1deddf2363b1a5738a0
Rafael Camison <rafaelcamison@gmail.com>
2023-06-02 01:49:44 +0200

    Task #795 - upgrade monolog

--- app/Logger/DatabaseHandler.php
+++ app/Logger/DatabaseHandler.php
@@ -4,26 +4,26 @@ namespace App\Logger;
 
 use App\Models\LogMessage;
 use Monolog\Handler\AbstractProcessingHandler;
-use Monolog\Logger;
+use Monolog\Level;
 use Monolog\LogRecord;
 
 class DatabaseHandler extends AbstractProcessingHandler
 {
 
-    public function __construct($level = Logger::DEBUG, bool $bubble = true)
+    public function __construct($level = Level::Debug, bool $bubble = true)
     {
         parent::__construct($level, $bubble);
     }
 
-    protected function write(array|LogRecord $record): void
+    protected function write(LogRecord $record): void
     {
         LogMessage::create([
-            'level' => $record['level'],
-            'level_name' => $record['level_name'],
-            'message' => $record['message'],
-            'logged_at' => $record['datetime'],
-            'context' => $record['context'],
-            'extra' => $record['extra'],
+            'level' => $record->level->value,
+            'level_name' => $record->level->name,
+            'message' => $record->message,
+            'logged_at' => $record->datetime,
+            'context' => $record->context,
+            'extra' => $record->extra,
         ]);
     }
 }
