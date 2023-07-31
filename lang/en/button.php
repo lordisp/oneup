9d8f2adc90dd28ba1ce651d14283c66c3ed9f437
Rafael Camison <rafaelcamison@gmail.com>
2023-07-31 10:29:31 +0200

    User Story #587 - UI enhancements Task #851 - fix button styles

--- lang/en/button.php
+++ lang/en/button.php
@@ -1,10 +1,11 @@
 <?php
 return [
     'clients_create' => 'Create a client',
-    'clients_bulk_delete' => 'Bulk delete provider',
+    'delete_selected' => 'Delete selected',
     'provider_create' => 'Create a provider',
     'provider_bulk_delete' => 'Bulk delete provider',
     'cancel' => 'Cancel',
+    'save' => 'Save',
     'operation_create' => 'Create a operation',
     'operations_bulk_delete' => 'Bulk delete operations',
     'role_create' => 'Create new role',
