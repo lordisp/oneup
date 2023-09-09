e94688217ba05b7406302b66e77125bf5bc60ebf
Rafael Camison <rafaelcamison@gmail.com>
2023-09-09 14:26:01 +0200

    Task #915 - remove unique interace

--- app/Jobs/DismissRiskyUsersScheduler.php
+++ app/Jobs/DismissRiskyUsersScheduler.php
@@ -7,14 +7,13 @@ use App\Services\AzureAD\RiskyUserTop;
 use App\Services\AzureAD\UserRiskState;
 use Illuminate\Bus\Batch;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class DismissRiskyUsersScheduler implements ShouldQueue, ShouldBeUnique
+class DismissRiskyUsersScheduler implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
