5af3a1c587f95e55b33f12957217acb99fe2d390
Shift <shift@laravelshift.com>
2024-03-19 11:58:02 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\TokenCacheServiceProvider::class,
+    App\Providers\AzureAD\UserServiceProvider::class,
+    App\Providers\AzureArm\ResourceGraphServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -5,29 +5,6 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
-        \Barryvdh\Debugbar\ServiceProvider::class,
-        App\Providers\TokenCacheServiceProvider::class,
-        App\Providers\AzureAD\UserServiceProvider::class,
-        App\Providers\AzureArm\ResourceGraphServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
