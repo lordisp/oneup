1039af475ca724f92fe43ac27c04dc338f7d69f9
Shift <shift@laravelshift.com>
2024-03-19 11:58:01 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,35 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->web([
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+        ]);
+
+        $middleware->api([
+            'throttle:api',
+            'client',
+        ]);
+
+        $middleware->group('client', [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+
+        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
+
+        $middleware->replaceInGroup('web', \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class, \App\Http\Middleware\VerifyCsrfToken::class);
+
+        $middleware->alias([
+            'client' => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
+            'client.scope' => \App\Http\Middleware\ClientScopesMiddleware::class,
+            'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+            'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+            'webhook' => \App\Http\Middleware\WebhookMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
